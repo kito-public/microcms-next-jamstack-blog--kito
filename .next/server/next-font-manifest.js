723711a20e7f817b168c81aa62cfeabebfa2d763
kito-public <kito.public@gmail.com>
2024-01-06 01:27:49 +0900

    Revert "test"
    This reverts commit c69d209750d85ce8c8edf47561be52fba96d4881.

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/itokai/Dropbox/htdocs/SANDBOX/microcms_nextjs/microcms-next-jamstack-blog--kito/src/app/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- src/app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <>
-      <title>404</title>
-      <h1>404</h1>
-    </>
-  );
-}
